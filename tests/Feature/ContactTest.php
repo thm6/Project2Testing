c2434d83d97b6d31b8df810eb96a5c7f6304ce1b
Tai <thm6@njit.edu>
2019-04-12 22:25:16 -0400

    Feature: Contact return a 200 status code.

---
+++ tests/Feature/ContactTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ContactTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testContact()
+    {
+        $response = $this->get('/contact');
+
+        $response->assertStatus(200);
+    }
+}
