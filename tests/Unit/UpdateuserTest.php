e287e7e1a9b17a4f42e7d54adbb7a30932cb49b0
Tai <thm6@njit.edu>
2019-04-12 22:53:37 -0400

    Unit: Update user test

---
+++ tests/Unit/UpdateuserTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UpdateuserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testUpdateuser()
+    {
+        $user = User::find(20);
+        $user->name = 'Steve Smith';
+        $user->email = 'steve@live.com';
+        $user->password = 'steve';
+        $this->assertTrue($user->save());
+    }
+
+}
